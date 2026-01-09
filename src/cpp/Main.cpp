0f1a619861eb84535395b35c6c264b811dde1da2
Patrick Hillen <phillen@uni-koblenz.de>
2026-01-09 10:16:36 +0100

    Refactor usage and name of function to run E2VID ('reconstruct' -> 'render')
    Refactor Main.cpp to prepare for calibration wrapper to be implemented

--- src/cpp/Main.cpp
+++ src/cpp/Main.cpp
@@ -37,7 +37,7 @@ int main (int argc, char *argv[])
 	std::signal(SIGINT, signalHandler);
 	std::signal(SIGTERM, signalHandler);
 
-	if (command == "calibrate")
+	if (command == "render")
 	{
 		std::string sessionPathStr;
 
@@ -49,7 +49,7 @@ int main (int argc, char *argv[])
 
 		if (sessionPathStr.empty())
 		{
-			Log::error("Error: Calibrate requires -s (session path).");
+			Log::error("Error: render requires -s (session path).");
 			logUsage(argv);
 			return EXIT_FAILURE;
 		}
@@ -85,6 +85,7 @@ int main (int argc, char *argv[])
 	else if (command == "record")
 	{		
 		std::string pathString;
+		std::string sessionName = "session_" + getCurrentTimestamp();
 		bool visualize = false;
 		for (int i = 2; i < argc; ++i)
 		{
@@ -105,6 +106,18 @@ int main (int argc, char *argv[])
                     return EXIT_FAILURE;		
 				}
 			}
+			else if (arg == "-n" || arg == "--name")
+			{
+				if (i + 1 < argc)
+				{
+					sessionName = "session_" + std::string(argv[++i]);
+				} else 
+				{
+					Log::error("Error: ", arg," flag requires a name argument.");
+                    logUsage(argv);
+                    return EXIT_FAILURE;		
+				}
+			}
 		}
 		
 		if (pathString.empty())
@@ -114,7 +127,6 @@ int main (int argc, char *argv[])
 			return EXIT_FAILURE;
 		}
 
-		std::string sessionName = "session_" + getCurrentTimestamp();
 		std::filesystem::path sessionDir = std::filesystem::path(pathString) / sessionName;
 
 		std::filesystem::path rawDir = sessionDir / "raw";
@@ -138,7 +150,73 @@ int main (int argc, char *argv[])
 			Log::info("Visualization enabled.");
 
 		return StereoRecorder::record(rawDir, visualize, stopSignal);	
-	} 
+	}
+	else if (command == "calibrate")
+	{
+		std::string sessionPathStr;
+
+		// int targetCols = 0;
+		// int targetRows = 0;
+		// float rowSpacingMeters = 0.0f;	
+		// float colSpacingMeters = 0.0f;
+		bool configProvided = false;
+
+        for (int i = 2; i < argc; ++i) 
+		{
+            std::string arg = argv[i];
+            if ((arg == "-s" || arg == "--session") && i + 1 < argc) sessionPathStr = argv[++i];
+            if ((arg == "-c" || arg == "--config") && i + 4 < argc)
+			{
+				try 
+				{
+					// targetCols = std::stoi(argv[++i]);    
+					//                targetRows = std::stoi(argv[++i]);    
+					//                rowSpacingMeters = std::stof(argv[++i]);    
+					//                colSpacingMeters = std::stof(argv[++i]);
+					// configProvided = true;
+				} catch (const std::exception& e) 
+				{
+					Log::error("Invalid numeric value in config: ", e.what());
+                    return EXIT_FAILURE;
+				}
+				
+			}
+        }
+
+		if (sessionPathStr.empty())
+		{
+			Log::error("Error: Calibrate requires -s (session path).");
+			logUsage(argv);
+			return EXIT_FAILURE;
+		}
+
+		if (!configProvided)
+		{
+				Log::error("Error: calibration requires --config <targetCols> <targetRows> <rowSpacingMeters> <colSpacingMeters> (checkerboard)");
+				Log::info("For more information, please refer to https://github.com/ethz-asl/kalibr/wiki/calibration-targets");
+				logUsage(argv);
+				return EXIT_FAILURE;		
+		}
+		
+		std::filesystem::path sessionDir(sessionPathStr);
+		std::filesystem::path rawDir = sessionDir / "raw";
+		std::filesystem::path intermediateDir = sessionDir / "intermediate";
+		std::filesystem::path reconstructionDir = sessionDir / "reconstruction";
+		std::filesystem::path configDir = sessionDir / "config";
+		std::filesystem::path calibrationDir = sessionDir / "calibration";
+
+		if (!std::filesystem::exists(reconstructionDir))
+		{
+			Log::error("Invalid session: 'reconstruction' directory missing in ", sessionDir.string());
+			return EXIT_FAILURE;
+		}
+
+		std::filesystem::create_directories(configDir);
+		std::filesystem::create_directories(calibrationDir);
+
+		Log::info("Initialized config/ and calibration/ directories for session: ", sessionPathStr);
+
+	}
 	else
 	{
 		logUsage(argv);
@@ -148,14 +226,30 @@ int main (int argc, char *argv[])
 }
 void logUsage(char* argv[])
 {
-	const std::string cmd = argv[0];
-	Log::error("Usage: ", cmd, " <command> [options]\n\n",
-			   "Commands:\n",
-			   "  record       Start recording\n",
-			   "  calibrate    Start calibration\n\n",
-			   "record Options:\n",
-			   "  -p, --path <path>   (Required) base output path for sessions\n",
-			   "  -v, --visualize     (Optional) enable preview\n\n",
-			   "calibrate Options:\n",
-			   "  -s, --session <path> (Required) path to session directory");
+    const std::string cmd = argv[0];
+    Log::info(
+        "Usage: ", cmd, " <command> [options]\n\n",
+
+        "Commands:\n",
+        "  record       Creates a timestamped session in <path> and saves raw .aedat4 data\n",
+        "  render       Processes raw data into frames/bags within the session directory\n",
+        "  calibrate    Computes intrinsics/extrinsics from frames and updates session config\n",
+        "  esvo         Runs 3D reconstruction and saves results to the session's esvo/ folder\n\n",
+
+        "record Options:\n",
+        "  -p, --path <dir>      (Required) Parent directory where 'session_YYYY-MM-DD..' or 'session_<name>' (if -n is provided) is created\n",
+		"  -n, --name			 (Optional) gives the session a name instead of the YYYY-MM-DD_H_M_S suffix\n",
+        "  -v, --visualize       (Optional) Enable live preview window\n\n",
+
+        "render Options:\n",
+        "  -s, --session <dir>   (Required) Path to the specific session folder to process\n\n",
+
+        "calibrate Options:\n",
+        "  -s, --session <dir>   (Required) Path to the session folder (outputs to /calibration/ and /config/)\n\n",
+
+        "esvo Options:\n",
+        "  -s, --session <dir>   (Required) Path to the session folder (outputs to /esvo/)\n\n",
+
+		"For more information about the session structure, take a look at https://github.com/patrickhln/event-stereo-reconstruction README.md\n"
+    );
 }
